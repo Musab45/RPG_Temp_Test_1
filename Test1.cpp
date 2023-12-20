b303e1328e16cf4f45069717a3393362d698efae
Musab <67883529+Musab45@users.noreply.github.com>
2023-12-20 19:39:05 +0500

    Update Test1.cpp

--- Test1.cpp
+++ Test1.cpp
@@ -39,22 +39,82 @@ void attack(Character& character, Enemy& enemy) {
 
 int main()
 {
-      // Initialize character and enemy
-  Character Mage = {"Mage", 100, 20};
-  Enemy Dragon = {"Dragon", 80, 15};
-  char choice;
-  do
-  {
-      
-      cout << "\nAttack(y/n): ";
-      cin >> choice;
-      // Print initial stats
-      cout << "\nYou: " << Mage.name << " (" << Mage.health << " HP, " << Mage.attackPower << " AP)" << endl;
-      cout << "Enemy: " << Dragon.name << " (" << Dragon.health << " HP, " << Dragon.attackPower << " AP)" << endl;
+    // Initialize character and enemy
+    Character Mage = { "Mage", 100, 20 };
+    Enemy Dragon = { "Dragon", 50, 15 };
 
-      // Simulate attack
-      attack(Mage, Dragon);
-  } while (choice == 'y');
+    char return_choice;
+    do
+    {
+        int menu_choice;
+        cout << "RPG GAME" << endl;
+        cout << "\nMenu" << endl;
+        cout << "1. Start a Quest!" << endl;
+        cout << "2. Go to Shop" << endl;
+        cout << "3. View Inventory" << endl;
+        cout << "Enter Selection: ";
+        cin >> menu_choice;
+        switch (menu_choice)
+        {
+        case 1:
+            //Quests
+            cout << "A wild Dragon appeared!" << endl;
 
-  return 0;
+            char choice;
+            do
+            {
+
+                cout << "\nAttack(y/n): ";
+                cin >> choice;
+                // Print initial stats
+                cout << "\nYou: " << Mage.name << " (" << Mage.health << " HP, " << Mage.attackPower << " AP)" << endl;
+                cout << "Enemy: " << Dragon.name << " (" << Dragon.health << " HP, " << Dragon.attackPower << " AP)" << endl;
+
+                // Simulate attack
+                attack(Mage, Dragon);
+                if (Mage.health <= 0 || Dragon.health <= 0)
+                {
+                    break;
+                }
+            } while (choice == 'y');
+
+            cout << "\nAnother wild Dragon appeared!" << endl;
+            do
+            {
+
+                cout << "\nAttack(y/n): ";
+                cin >> choice;
+                // Print initial stats
+                cout << "\nYou: " << Mage.name << " (" << Mage.health << " HP, " << Mage.attackPower << " AP)" << endl;
+                cout << "Enemy: " << Dragon.name << " (" << Dragon.health << " HP, " << Dragon.attackPower << " AP)" << endl;
+
+                // Simulate attack
+                attack(Mage, Dragon);
+                if (Mage.health <= 0 || Dragon.health <= 0)
+                {
+                    break;
+                }
+            } while (choice == 'y');
+
+            break;
+
+        case 2:
+            //Shop
+            cout << "2";
+            break;
+
+        case 3:
+            //View Inventory
+            cout << "3";
+            break;
+
+        default:
+            //Default Case
+            cout << "4";
+        }
+        cout << "\nReturn to Menu?(y/n): ";
+        cin >> return_choice;
+    } while (return_choice == 'y');
+
+    return 0;
 }
