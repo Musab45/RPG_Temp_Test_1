4baf0de60a4fa90304b201ad85bb95838a72e5aa
Musab <67883529+Musab45@users.noreply.github.com>
2023-12-20 23:36:49 +0500

    Update Test1.cpp

--- Test1.cpp
+++ Test1.cpp
@@ -1,38 +1,48 @@
 #include<iostream>
 using namespace std;
 
-struct Character {
+struct Character 
+{
     string name;
-    int health;
-    int attackPower;
+    int hp;
+    int atk;
+    int exp;
 };
 
-struct Enemy {
+struct Enemy
+{
     string name;
-    int health;
-    int attackPower;
+    int hp;
+    int atk;
 };
 
+string selected_char_name;
+int selected_char_hp;
+int selected_char_atk;
 
 // Function to handle character and enemy attack
 void attack(Character& character, Enemy& enemy) {
     // Character attacks enemy
-    enemy.health -= character.attackPower;
-    cout << character.name << " attacks " << enemy.name << " for " << character.attackPower << " damage!" << endl;
+    enemy.hp -= character.atk;
+    cout << endl;
+    cout << "** " << character.name << " attacks " << enemy.name << " with " << character.atk << " damage! **" << endl;
 
     // Check if enemy is defeated
-    if (enemy.health <= 0) {
-        cout << enemy.name << " is defeated!" << endl;
+    if (enemy.hp <= 0) {
+        cout << endl;
+        cout <<"** " << enemy.name << " is defeated! **" << endl;
     }
 
     // Enemy attacks character (if still alive)
-    if (enemy.health > 0) {
-        character.health -= enemy.attackPower;
-        cout << enemy.name << " attacks back with " << enemy.attackPower << " damage!" << endl;
+    if (enemy.hp > 0) {
+        character.hp -= enemy.atk;
+        cout << endl;
+        cout << "** " << enemy.name << " attacks back with " << enemy.atk << " damage! **" << endl;
 
         // Check if character is defeated
-        if (character.health <= 0) {
-            cout << character.name << " is defeated!" << endl;
+        if (character.hp <= 0) {
+            cout << endl;
+            cout << "** " << character.name << " is defeated! **" << endl;
         }
     }
 }
@@ -40,60 +50,126 @@ void attack(Character& character, Enemy& enemy) {
 int main()
 {
     // Initialize character and enemy
-    Character Mage = { "Mage", 100, 20 };
+    Character Warrior = { "Warrior", 100, 15 };
+    Character Mage = { "Mage", 200, 10 };
+    Character Rouge = { "Rouge", 70, 70 };
     Enemy Dragon = { "Dragon", 50, 15 };
 
+    //Selected Character;
+    Character Selected_Char = { selected_char_name,selected_char_hp,selected_char_atk };
+
+    //Start
     char return_choice;
     do
     {
         int menu_choice;
         cout << "RPG GAME" << endl;
         cout << "\nMenu" << endl;
-        cout << "1. Start a Quest!" << endl;
+        cout << "\n1. Start a Quest!" << endl;
         cout << "2. Go to Shop" << endl;
         cout << "3. View Inventory" << endl;
-        cout << "Enter Selection: ";
+        cout << "\nEnter Selection: ";
         cin >> menu_choice;
         switch (menu_choice)
         {
         case 1:
             //Quests
-            cout << "A wild Dragon appeared!" << endl;
+            int char_select;
+            cout << endl;
+            cout << "Select a Character: " << endl;
+            cout << endl;
+            cout << "1. Warrior" << endl;
+            cout << "2. Mage" << endl;
+            cout << "3. Rouge" << endl;
+            cout << endl;
+            cout << "Selection: ";
+            cin >> char_select;
+            switch (char_select)
+            {
+            case 1: 
+                Selected_Char.name = Warrior.name;
+                Selected_Char.hp = Warrior.hp;
+                Selected_Char.atk = Warrior.atk;
+                break;
+            case 2:
+                Selected_Char.name = Mage.name;
+                Selected_Char.hp = Mage.hp;
+                Selected_Char.atk = Mage.atk;
+                break;
+            case 3:
+                Selected_Char.name = Rouge.name;
+                Selected_Char.hp = Rouge.hp;
+                Selected_Char.atk = Rouge.atk;
+                break;
+            default:
+                cout << endl;
+                cout << "Wrong Entry!";
+                break;
+            }
+            cout << endl;
+            cout << "A Lvl 1 Wild Dragon Appeared!" << endl;
 
             char choice;
             do
             {
-
+                // Print initial stats
+                //cout << "\nYou: " << Mage.name << " (" << Mage.hp << " HP, " << Mage.attack << " AP)" << endl;
+                cout << endl;
+                cout << "Your Stats: " << endl;
+                cout << " -> "<< Selected_Char.name << endl;
+                cout << "  -> HP: " << Selected_Char.hp << endl;
+                cout << "  -> ATK: " << Selected_Char.atk << endl;
+                //cout << "\nEnemy: " << Dragon.name << " (" << Dragon.hp << " HP, " << Dragon.atk << " AP)" << endl;
+                cout << endl;
+                cout << "Enemy Stats: " << endl;
+                cout << "  -> HP: " << Dragon.hp << endl;
+                cout << "  -> ATK: " << Dragon.atk << endl;
                 cout << "\nAttack(y/n): ";
                 cin >> choice;
-                // Print initial stats
-                cout << "\nYou: " << Mage.name << " (" << Mage.health << " HP, " << Mage.attackPower << " AP)" << endl;
-                cout << "Enemy: " << Dragon.name << " (" << Dragon.health << " HP, " << Dragon.attackPower << " AP)" << endl;
 
                 // Simulate attack
-                attack(Mage, Dragon);
-                if (Mage.health <= 0 || Dragon.health <= 0)
+                attack(Selected_Char, Dragon);
+                //Exit if Enemy/Player Dies
+                if (Selected_Char.hp <= 0 || Dragon.hp <= 0)
                 {
                     break;
                 }
-            } while (choice == 'y');
 
-            cout << "\nAnother wild Dragon appeared!" << endl;
+            } while (choice == 'y');
+            Dragon.hp = 50;
+
+            //Second Dragon
+            cout << "Another Lvl 1 Wild Dragon Appeared!" << endl;
             do
             {
+                // Print initial stats
+                
+                //cout << "\nYou: " << Mage.name << " (" << Mage.hp << " HP, " << Mage.attack << " AP)" << endl;
 
+                cout << endl;
+                cout << "Your Stats: " << endl;
+                cout << " -> " << Selected_Char.name << endl;
+                cout << "  -> HP: " << Selected_Char.hp << endl;
+                cout << "  -> ATK: " << Selected_Char.atk << endl;
+
+                //cout << "\nEnemy: " << Dragon.name << " (" << Dragon.hp << " HP, " << Dragon.atk << " AP)" << endl;
+
+                cout << endl;
+                cout << "Enemy Stats: " << endl;
+                cout << "  -> HP: " << Dragon.hp << endl;
+                cout << "  -> ATK: " << Dragon.atk << endl;
                 cout << "\nAttack(y/n): ";
                 cin >> choice;
-                // Print initial stats
-                cout << "\nYou: " << Mage.name << " (" << Mage.health << " HP, " << Mage.attackPower << " AP)" << endl;
-                cout << "Enemy: " << Dragon.name << " (" << Dragon.health << " HP, " << Dragon.attackPower << " AP)" << endl;
 
                 // Simulate attack
-                attack(Mage, Dragon);
-                if (Mage.health <= 0 || Dragon.health <= 0)
+                attack(Selected_Char, Dragon);
+
+                //Exit if Enemy/Player Dies
+                if (Selected_Char.hp <= 0 || Dragon.hp <= 0)
                 {
                     break;
                 }
+
             } while (choice == 'y');
 
             break;
